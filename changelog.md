c0bf8243f41b0b2814b2cb2e20c5195bfe848953
Michael Theis (msg) <michael.theis@msg.group>
2023-05-30 12:32:39 +0200

    released version 3.1.0

--- changelog.md
+++ changelog.md
@@ -6,6 +6,11 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+## [3.1.0] - 2023-05-26
+### Changed
+- upgraded to Spring Boot 3.1.0
+- switched from jaeger tracing to micrometer tracing via open telemetry
+
 ## [3.0.0] - 2023-05-25
 ### Changed
 - upgraded to Java 17
--- src/main/java/group/msg/at/cloud/cloudtrain/SpringDownstreamApplication.java
+++ src/main/java/group/msg/at/cloud/cloudtrain/SpringDownstreamApplication.java
@@ -1,13 +1,12 @@
 package group.msg.at.cloud.cloudtrain;
 
-import group.msg.at.cloud.common.observability.CommonObservability;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * Point of entry for this Spring Boot application.
  */
-@SpringBootApplication(scanBasePackageClasses = {SpringDownstreamApplication.class, CommonObservability.class})
+@SpringBootApplication
 public class SpringDownstreamApplication {
 
     public static void main(String[] args) {
